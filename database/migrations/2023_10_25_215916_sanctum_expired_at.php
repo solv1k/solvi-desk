d5bce7f35041291c5541ed312a229b416108db57
Vladimir Solvi <solvik74@mail.ru>
2023-10-26 03:12:05 +0500

    update sanctum

---
+++ database/migrations/2023_10_25_215916_sanctum_expired_at.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('personal_access_tokens', static function (Blueprint $table): void {
+            $table->timestamp('expires_at')->nullable()->after('last_used_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('personal_access_tokens', static function (Blueprint $table): void {
+            $table->dropColumn('expires_at');
+        });
+    }
+};
